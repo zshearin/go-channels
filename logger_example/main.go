864708cd39af9060828b97c00e2df05fe7055d89
Zach Shearin <zach.shearin@ibm.com>
2020-01-09 11:19:20 -0500

    change formatting of timestamp

--- logger_example/main.go
+++ logger_example/main.go
@@ -34,7 +34,7 @@ func logger() {
 	for {
 		select {
 		case entry := <-logCh:
-			fmt.Printf("%v - [%v] %v\n", entry.time.String(), entry.severity, entry.message)
+			fmt.Printf("%v - [%v] %v\n", entry.time.Format("2006-01-02 15:04:05"), entry.severity, entry.message)
 		case <-doneCh:
 			break
 		}
